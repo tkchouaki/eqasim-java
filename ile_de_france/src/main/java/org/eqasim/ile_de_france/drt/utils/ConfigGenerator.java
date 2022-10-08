76372509e36cf35b4e2b9b19d340bfda08dc4821
Tarek Chouaki <tarek.chouaki@irt-systemx.fr>
2022-10-08 15:58:00 +0200

    Updated ConfigGenerator

--- ile_de_france/src/main/java/org/eqasim/ile_de_france/drt/utils/ConfigGenerator.java
+++ ile_de_france/src/main/java/org/eqasim/ile_de_france/drt/utils/ConfigGenerator.java
@@ -4,6 +4,7 @@ import jogamp.graph.font.typecast.ot.table.ID;
 import org.apache.commons.io.FileUtils;
 import org.eqasim.ile_de_france.drt.IDFDrtConfigGroup;
 import org.eqasim.ile_de_france.drt.IDFDrtConfigurator;
+import org.eqasim.ile_de_france.drt.mode_choice.utilities.drt_rejection_penalty.DrtRejectionPenaltyProviderConfigGroup;
 import org.eqasim.ile_de_france.drt.mode_choice.utilities.drt_rejection_penalty.DrtRejectionsLinearPenaltyProviderConfigGroup;
 import org.matsim.core.config.CommandLine;
 import org.matsim.core.config.Config;
@@ -11,7 +12,11 @@ import org.matsim.core.config.ConfigUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Map;
 
 public class ConfigGenerator {
@@ -42,7 +47,16 @@ public class ConfigGenerator {
             assert this.hasNext();
             this.increaseIndex();
             IDFDrtConfigGroup idfDrtConfigGroup = (IDFDrtConfigGroup) baseConfig.getModules().get(IDFDrtConfigGroup.GROUP_NAME);
-            DrtRejectionsLinearPenaltyProviderConfigGroup linearPenaltyProviderConfigGroup = (DrtRejectionsLinearPenaltyProviderConfigGroup) idfDrtConfigGroup.getDrtRejectionPenaltyProviderConfig().getPenaltyProviderParams();
+            DrtRejectionPenaltyProviderConfigGroup penaltyProviderConfigGroup = idfDrtConfigGroup.getDrtRejectionPenaltyProviderConfig();
+            DrtRejectionsLinearPenaltyProviderConfigGroup linearPenaltyProviderConfigGroup = null;
+            if(penaltyProviderConfigGroup != null) {
+                linearPenaltyProviderConfigGroup = (DrtRejectionsLinearPenaltyProviderConfigGroup) penaltyProviderConfigGroup.getPenaltyProviderParams();
+            } else {
+                linearPenaltyProviderConfigGroup = new DrtRejectionsLinearPenaltyProviderConfigGroup();
+                penaltyProviderConfigGroup = new DrtRejectionPenaltyProviderConfigGroup();
+                penaltyProviderConfigGroup.addParameterSet(linearPenaltyProviderConfigGroup);
+                idfDrtConfigGroup.addParameterSet(penaltyProviderConfigGroup);
+            }
             linearPenaltyProviderConfigGroup.setAlpha(this.alphaValues[this.index[0]]);
             linearPenaltyProviderConfigGroup.setInitialRejectionPenalty(this.initialPenaltyValues[this.index[1]]);
             linearPenaltyProviderConfigGroup.setTargetRejectionProbability(this.targetPenaltyValues[this.index[2]]);
@@ -68,13 +82,22 @@ public class ConfigGenerator {
                     return this.index[0] < alphaValues.length-1;
                 }
             }
-            return false;
+            return true;
         }
+    }
+
+    public static void replaceInFile(String filePath, String toReplace, String replacement) throws IOException {
+        Path path = Paths.get(filePath);
+        Charset charset = StandardCharsets.UTF_8;
+
+        String content = Files.readString(path, charset);
+        content = content.replaceAll(toReplace, replacement);
+        Files.write(path, content.getBytes(charset));
     }
 
     public static void main(String[] args) throws CommandLine.ConfigurationException, IOException {
         CommandLine cmd = new CommandLine.Builder(args) //
-                .requireOptions("base-config-path", "base-output-dir")
+                .requireOptions("base-config-path", "base-output-dir", "replace-in-file")
                 .build();
 
         ConfigGenerationInterface configGenerator = new DrtRejectionLinearPenaltyProviderSensitivityGenerator(new double[]{0.0}, new double[]{0, 0.05, 0.1}, new double[]{ 0.05, 0.1, 0.3, 0.5, 0.9});
@@ -84,12 +107,19 @@ public class ConfigGenerator {
         Path baseOutputDirPath = Path.of(cmd.getOptionStrict("base-output-dir"));
         IDFDrtConfigurator configurator = new IDFDrtConfigurator();
         Config config = ConfigUtils.loadConfig(baseConfigPath.toAbsolutePath().toString(), configurator.getConfigGroups());
+        String[] replacementArgs = new String[0];
+        if(cmd.hasOption("replace-in-file")){
+            replacementArgs = cmd.getOptionStrict("replace-in-file").split(",");
+        }
 
         while(configGenerator.hasNext()) {
             String suffix = configGenerator.next(config);
             Path generatedDirPath = baseOutputDirPath.resolve(baseDirectoryPath.getFileName().toString() + suffix);
             FileUtils.copyDirectory(new File(baseDirectoryPath.toAbsolutePath().toString()), new File(generatedDirPath.toAbsolutePath().toString()));
             ConfigUtils.writeConfig(config, generatedDirPath.resolve(baseConfigPath.getFileName()).toAbsolutePath().toString());
+            if(replacementArgs.length == 2) {
+                replaceInFile(generatedDirPath.resolve(replacementArgs[0]).toAbsolutePath().toString(), replacementArgs[1], replacementArgs[1]+suffix);
+            }
         }
     }
 }
