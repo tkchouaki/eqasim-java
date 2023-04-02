aae80b7d9af9b69ce95fbf0f3957eb24383b1dbd
Tarek Chouaki <tarek.chouaki@irt-systemx.fr>
2023-04-03 00:57:21 +0200

    Fixing analysis bug

--- ile_de_france/src/main/java/org/eqasim/ile_de_france/feeder/analysis/FeederAnalysisModule.java
+++ ile_de_france/src/main/java/org/eqasim/ile_de_france/feeder/analysis/FeederAnalysisModule.java
@@ -2,12 +2,14 @@ package org.eqasim.ile_de_france.feeder.analysis;
 
 import com.google.inject.Singleton;
 import org.matsim.core.controler.AbstractModule;
+import org.matsim.core.router.MainModeIdentifier;
 
 public class FeederAnalysisModule extends AbstractModule {
 
     @Override
     public void install() {
         bind(FeederAnalysisListener.class).in(Singleton.class);
+        bind(MainModeIdentifier.class).to(FeederMainModeIdentifier.class);
         addControlerListenerBinding().to(FeederAnalysisListener.class);
     }
 }
